e813cb6d9ed0912c2ef8b1d9337c0a6dfd015240
pragyanmaharjan63 <pragyanmaharjan6k@gmail.com>
2026-01-20 09:11:51 +0545

    feat(UI): fixed responsiveness of login and added showpw button

--- frontend/src/components/ui/form.tsx
+++ frontend/src/components/ui/form.tsx
@@ -1,5 +1,5 @@
 import { useForm, type SubmitHandler } from "react-hook-form";
-
+import { Eye, EyeClosed } from "lucide-react";
 type Inputs = {
   example: string;
   exampleRequired: string;
@@ -7,8 +7,10 @@ type Inputs = {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Separator } from "./separator";
+import { useState } from "react";
 
 export default function Form() {
+  const [showpw, setShowpw] = useState(false);
   const {
     register,
     handleSubmit,
@@ -16,31 +18,43 @@ export default function Form() {
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="grid gap-3  rounded-xl bg-white p-5 shadow-lg">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="grid gap-3 rounded-xl bg-white p-5 shadow-lg w-max"
+    >
+      <div className="">
         <div className="grid place-items-center">
-          <p className="text-xl sm:text-2xl font-bold ">LOG IN</p>
-          <p className="text-xs sm:text-sm ">Welcome Back</p>
-          <p className="text-xs sm:text-sm ">Please login to continue</p>
+          <p className="text-2xl font-bold ">LOG IN</p>
+          <p className="text-sm ">Welcome Back</p>
+          <p className="text-sm ">Please login to continue</p>
         </div>
-        <div className="grid w-[20vw] gap-4">
+        <div className="grid w-60 lg:w-[20vw] gap-4">
           <div>
             <p className="px-1 font-normal ">Username</p>
             <Input {...register("example")} placeholder="Username" />
           </div>
           <div>
             <p className="px-1 font-normal ">Password</p>
-            <Input
-              {...register("exampleRequired", { required: true })}
-              placeholder="Password"
-            />
+            <span className="relative">
+              <Input
+                {...register("exampleRequired", { required: true })}
+                placeholder="Password"
+                type={showpw ? "text" : "password"}
+              />
+              <span
+                className="absolute top-0 right-4 cursor-pointer"
+                onClick={() => setShowpw((prev) => !prev)}
+              >
+                {!showpw && <EyeClosed />}
+                {showpw && <Eye />}
+              </span>
+            </span>
           </div>
           {errors.exampleRequired && (
             <span className="text-sm text-red-500">This field is required</span>
           )}
-          <div className="flex gap-1 px-1 opacity-55">
-            <p>Don't have an account? </p>
-            <p>Sign up</p>
+          <div className="flex text-sm gap-1 px-1 opacity-55">
+            Don't have an account? <p>Sign up</p>
           </div>
           <Button className="w-full cursor-pointer" type="submit">
             Submit
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -2,15 +2,15 @@ import Form from "@/components/ui/form";
 
 export default function Login() {
   return (
-    <div className="grid relative md:grid-cols-2 place-items-center h-screen bg-[url('/loginDeco.png')] bg-cover">
-      <div className="">
+    <div className="grid relative lg:grid-cols-3 place-items-center h-screen bg-[url('/loginDeco.png')] bg-cover">
+      <div className="grid place-items-center lg:col-span-2 w-full">
         <Form />
       </div>
       <div className="hidden lg:block">
         <img
           src="/logo.png"
           alt="logo"
-          className="absolute w-80 lg:w-auto right-[2vw] lg:right-[6vw] top-[40vh]"
+          className="absolute w-80   right-[2vw] lg:columns-1 lg:right-[6vw] top-[40vh]"
         />
       </div>
     </div>
